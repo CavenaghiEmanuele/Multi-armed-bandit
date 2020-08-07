0db438f49e072afa36a14346b757473bee6c4fa2
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-08-08 00:17:35 +0200

    Add decay rate for gaussian algorithms

--- multi_armed_bandit/algorithms/gaussian_dist/gaussian_greedy.py
+++ multi_armed_bandit/algorithms/gaussian_dist/gaussian_greedy.py
@@ -5,8 +5,8 @@ import numpy as np
 
 class GaussianGreedy(GaussianAlgo):
 
-    def __init__(self, n_arms: int):
-        super().__init__(n_arms)
+    def __init__(self, n_arms: int, decay_rate: float = 0.01):
+        super().__init__(n_arms, decay_rate)
 
     def select_action(self) -> int:
         return np.argmax(self._mu)
--- multi_armed_bandit/algorithms/gaussian_dist/gaussian_ts.py
+++ multi_armed_bandit/algorithms/gaussian_dist/gaussian_ts.py
@@ -6,8 +6,8 @@ from numpy.random import normal
 
 class GaussianThompsonSampling(GaussianAlgo):
 
-    def __init__(self, n_arms: int):
-        super().__init__(n_arms)
+    def __init__(self, n_arms: int, decay_rate: float = 0.01):
+        super().__init__(n_arms, decay_rate)
 
     def select_action(self) -> int:
         samples = [normal(loc=self._mu[a], scale=self._dev_std[a]) for a in range(self._n_arms)]
