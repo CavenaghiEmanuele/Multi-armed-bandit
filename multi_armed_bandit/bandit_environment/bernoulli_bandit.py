9b80818317a1b54e7a41e3ab0abcf2434181bb3e
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-08-08 14:45:00 +0200

    Update plot_arms function

--- multi_armed_bandit/bandit_environment/bernoulli_bandit.py
+++ multi_armed_bandit/bandit_environment/bernoulli_bandit.py
@@ -32,12 +32,13 @@ class BernoulliBandit(MultiArmedBandit):
             "Probabilities = " + str(self._probabilities)
     
     def plot_arms(self):
+        fig = plt.figure()
         for a in range(self._n_arms):
-            plt.bar(a, self._probabilities[a], label="Action: " + str(a) + ", prob: " + str(self._probabilities[a]))
-
-        plt.legend()
-        plt.show()
-                    
+            _ = plt.bar(a, self._probabilities[a], label="Action: " + str(a) + ", prob: " + str(self._probabilities[a]))
+        fig.suptitle("Bandit's arms")
+        fig.legend()
+        fig.show()
+                         
     def do_action(self, action: int):
         return binomial(size=1, n=1, p= self._probabilities[action])
     
