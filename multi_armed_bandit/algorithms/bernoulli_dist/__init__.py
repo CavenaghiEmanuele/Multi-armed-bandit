3d35e21ea0950bd2d2c9ae716faa132c719cec11
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-08-08 23:07:49 +0200

    Add bernoulli UCB algorithm

--- multi_armed_bandit/algorithms/bernoulli_dist/__init__.py
+++ multi_armed_bandit/algorithms/bernoulli_dist/__init__.py
@@ -1,3 +1,4 @@
 from .bernoulli_algo import BernoulliAlgo
 from .bernoulli_greedy import BernoulliGreedy
 from .bernoulli_ts import BernoulliThompsonSampling
+from .bernoulli_ucb import BernoulliUCB
---
+++ multi_armed_bandit/algorithms/bernoulli_dist/bernoulli_ucb.py
@@ -0,0 +1,32 @@
+from .bernoulli_algo import BernoulliAlgo
+
+import numpy as np
+from math import log, sqrt
+from typing import List
+
+
+class BernoulliUCB(BernoulliAlgo):
+
+    _action_taken: int
+    _action_selection: List
+    _c: float
+
+    def __init__(self, n_arms: int, c: float=1):
+        super().__init__(n_arms)
+        self._action_taken = 0
+        self._action_selection = [0 for _ in range(n_arms)]
+        self._c = c
+
+    def select_action(self) -> int:
+        self._action_taken += 1
+        estimates = []
+        for a in range(self._n_arms):
+            if self._action_selection[a] == 0:
+                estimates.append(float("inf"))
+            else:
+                estimates.append(self._betas[a][0] / (self._betas[a][0] + self._betas[a][1]) + 
+                                 self._c * sqrt(log(self._action_taken) / self._action_selection[a]))
+
+        action = np.argmax(estimates)
+        self._action_selection[action] += 1
+        return action
