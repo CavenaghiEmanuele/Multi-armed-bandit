7834667fd0ec53746a712369fb11f54c8540eb21
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-08-09 00:11:08 +0200

    Add first version of bernoulli dynamic env

--- multi_armed_bandit/bandit_environment/__init__.py
+++ multi_armed_bandit/bandit_environment/__init__.py
@@ -1,3 +1,4 @@
 from .multi_armed_bandit import MultiArmedBandit
 from .gaussian_bandit import GaussianBandit
 from .bernoulli_bandit import BernoulliBandit
+from .bernoulli_dynamic_bandit import BernoulliDynamicBandit
---
+++ multi_armed_bandit/bandit_environment/bernoulli_dynamic_bandit.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.random import uniform, binomial
+from typing import List, Dict
+
+from . import BernoulliBandit
+
+
+class BernoulliDynamicBandit(BernoulliBandit):
+
+    _probability_of_change: float
+    _action_selection: Dict
+
+    def __init__(self, n_arms: int, probabilities: List[float] = None, probability_of_change: float = 0.001):
+        super().__init__(n_arms, probabilities)
+        self._probability_of_change = probability_of_change
+        self._action_selection = {a:[self._probabilities[a]] for a in range(n_arms)}
+
+    def plot_arms(self):
+        plt.figure()
+        for a in range(self._n_arms):
+            plt.plot(self._action_selection[a], label="Action: " +
+                        str(a) + ", prob: " + str(self._probabilities[a]))
+        plt.suptitle("Bandit's arms")
+        plt.legend()
+        plt.show()
+
+    def _change_action_probabilities(self):
+        for action in range(self._n_arms):
+            if uniform(0, 1) < self._probability_of_change:
+                self._probabilities[action] = uniform(0, 1)
+                self._best_action_mean = np.max(self._probabilities)
+            self._action_selection[action].append(self._probabilities[action])
+
+    def do_action(self, action: int):
+        reward = binomial(size=1, n=1, p=self._probabilities[action])
+        self._change_action_probabilities()
+        return reward
