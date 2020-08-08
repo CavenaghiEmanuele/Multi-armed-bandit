971b2577515545325c229f18197b180ddda821a4
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-08-08 14:33:30 +0200

    Fix bugs and add plot_estimates function

--- multi_armed_bandit/algorithms/gaussian_dist/gaussian_algo.py
+++ multi_armed_bandit/algorithms/gaussian_dist/gaussian_algo.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 from typing import List, Tuple
 from abc import ABC, abstractmethod
@@ -8,26 +10,38 @@ class GaussianAlgo(ABC):
 
     _n_arms: int
     _mu: List[float]
-    _dev_std: List[float]
-    _n_action_taken: int
+    _std_dev: List[float]
+    _n_action_taken: List[int]
 
-    def __init__(self, n_arms: int, decay_rate: float = 0.01):
+    def __init__(self, n_arms: int, decay_rate: float = 0.99):
         super().__init__()
 
         self._n_arms = n_arms
-        self._mu = np.zeros(n_arms)
-        self._dev_std = np.ones(n_arms)
+        self._mu = np.ones(n_arms)
+        self._std_dev = np.ones(n_arms)
         self._decay_rate = decay_rate
-        self._n_action_taken = 0
+        self._n_action_taken = np.zeros(n_arms)
 
     def update_estimates(self, action: int, reward: int) -> None:
-        self._n_action_taken += 1
-        n = self._n_action_taken
-        old_mean = self._mu[action]
-        self._mu[action] += (1 / n) * (reward - old_mean)
-        self._dev_std[action] *= self._decay_rate
+        self._n_action_taken[action] += 1
+        self._std_dev[action] *= self._decay_rate
+        n = self._n_action_taken[action]
+        if n == 0:
+            self._mu[action] = reward
+            return
+        self._mu[action] += (1 / n) * (reward - self._mu[action])
         
-
     @abstractmethod
     def select_action(self) -> int:
         pass
+    
+    def plot_estimates(self):  
+        fig = plt.figure()
+        for a in range(self._n_arms):
+            x = np.linspace(self._mu[a] - 3*self._std_dev[a], self._mu[a] + 3*self._std_dev[a])
+            _ = plt.plot(x, 
+                     stats.norm.pdf(x, self._mu[a], self._std_dev[a]), 
+                     label="Action: " + str(a) + ", Mean: " + str(self._mu[a]) + ", std_dev: " + str(self._std_dev[a]))
+        fig.suptitle("Action's estimates")
+        fig.legend()
+        fig.show()
