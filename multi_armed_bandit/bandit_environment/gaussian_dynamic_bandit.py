b065f16c17edd9c2ed1712b677b92fc6dc4cdd72
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-08-10 23:34:13 +0200

    Add fixed action value

--- multi_armed_bandit/bandit_environment/gaussian_dynamic_bandit.py
+++ multi_armed_bandit/bandit_environment/gaussian_dynamic_bandit.py
@@ -10,11 +10,18 @@ class GaussianDynamicBandit(GaussianBandit):
 
     _probability_of_change: float
     _action_selection: Dict
+    _fixed_actions: List = []
 
-    def __init__(self, n_arms: int, mean: List[float] = None, std_dev: List[float] = None, probability_of_change: float = 0.001):
+    def __init__(self, n_arms: int, mean: List[float] = None, std_dev: List[float] = None, 
+                 probability_of_change: float = 0.001, fixed_action_prob: float = None):
         super().__init__(n_arms, mean, std_dev)
         self._probability_of_change = probability_of_change
         self._action_selection = {a:[self._mean[a]] for a in range(n_arms)}
+        
+        if fixed_action_prob != None:
+            for i in range(n_arms):
+                if uniform(0, 1) < fixed_action_prob:
+                    self._fixed_actions.append(i)
 
     def plot_arms(self, render: bool = True):  
         plt.figure()
@@ -28,7 +35,7 @@ class GaussianDynamicBandit(GaussianBandit):
 
     def _change_action_probabilities(self):
         for action in range(self._n_arms):
-            if uniform(0, 1) < self._probability_of_change:
+            if (not action in self._fixed_actions) and (uniform(0, 1) < self._probability_of_change):
                 self._mean[action] = uniform(0, 1)
                 self._best_action_mean = np.max(self._mean)
             self._action_selection[action].append(self._mean[action])
