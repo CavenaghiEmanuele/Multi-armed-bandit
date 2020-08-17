48f73d280d4519f728646c61715d01f2106f34aa
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-08-17 22:24:29 +0200

    Update plot estimation

--- multi_armed_bandit/algorithms/bernoulli_dist/bernoulli_algo.py
+++ multi_armed_bandit/algorithms/bernoulli_dist/bernoulli_algo.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from typing import List, Tuple
-from abc import ABC, abstractmethod
+from typing import List, Dict
+from abc import ABC
 
 from ..algorithm import Algorithm
 
@@ -10,27 +10,31 @@ from ..algorithm import Algorithm
 class BernoulliAlgo(Algorithm, ABC):
 
     _betas: np.ndarray
+    _mean_trace: Dict
 
     def __init__(self, n_arms: int):
         super().__init__(n_arms=n_arms)
         self._betas = np.ones(shape=(n_arms, 2))
-        
+        self._mean_trace = {action : [1/2] for action in range(n_arms)}
+
     def reset_agent(self):
         self._betas = np.ones(shape=(self._n_arms, 2))
+        self._mean_trace = {action : [1/2] for action in range(self._n_arms)}
 
     def update_estimates(self, action: int, reward: int) -> None:
         if reward == 0:
             self._betas[action][1] += 1  # Update beta
         else:  # Reward == 1
             self._betas[action][0] += 1  # Update alpha
+        for a in range(self._n_arms):
+            self._mean_trace[a].append(self._betas[a][0] / (self._betas[a][0] + self._betas[a][1]))
 
-    def plot_estimates(self):  
+    def plot_estimates(self, render: bool = True):
         fig = plt.figure()
         for a in range(self._n_arms):
-            _ = plt.bar(a, self._betas[a][0] / (self._betas[a][0] + self._betas[a][1]), label="Action: " + str(a) + 
-                        ", Mean: " + str(self._betas[a][0] / (self._betas[a][0] + self._betas[a][1])) + 
-                        ", Parms: " + str(self._betas[a]))
-                        
+            _ = plt.plot(self._mean_trace[a], label="Action: " + str(a))
+
         fig.suptitle("Action's estimates")
         fig.legend()
-        fig.show()
+        if render:
+            fig.show()
--- multi_armed_bandit/algorithms/bernoulli_dist/dynamic_bernoulli_ts.py
+++ multi_armed_bandit/algorithms/bernoulli_dist/dynamic_bernoulli_ts.py
@@ -10,7 +10,7 @@ from . import BernoulliAlgo
 From Paper: "Taming Non-stationary Bandits: A Bayesian Approach"
 '''
 class DynamicBernoulliTS(BernoulliAlgo, ABC):
-    
+
     _alpha_0: float
     _beta_0: float
     _gamma: float
@@ -20,7 +20,7 @@ class DynamicBernoulliTS(BernoulliAlgo, ABC):
         self._alpha_0 = alpha_0
         self._beta_0 = beta_0
         self._gamma = gamma
-        
+
     def __repr__(self):
         return "Dynamic Thompson Sampling Bernoulli"
 
@@ -30,20 +30,10 @@ class DynamicBernoulliTS(BernoulliAlgo, ABC):
             self._betas[action][1] += 1  # Update beta
         else:  # Reward == 1
             self._betas[action][0] += 1  # Update alpha
-            
-            
+        for a in range(self._n_arms):
+            self._mean_trace[a].append(self._betas[a][0] / (self._betas[a][0] + self._betas[a][1]))
+
     def select_action(self) -> int:
         samples = [beta(a=self._betas[a][0] + self._alpha_0, b=self._betas[a][1] + self._beta_0)
                    for a in range(self._n_arms)]
         return np.argmax(samples)
-
-    def plot_estimates(self):  
-        fig = plt.figure()
-        for a in range(self._n_arms):
-            _ = plt.bar(a, self._betas[a][0] / (self._betas[a][0] + self._betas[a][1]), label="Action: " + str(a) + 
-                        ", Mean: " + str(self._betas[a][0] / (self._betas[a][0] + self._betas[a][1])) + 
-                        ", Parms: " + str(self._betas[a]))
-                        
-        fig.suptitle("Action's estimates")
-        fig.legend()
-        fig.show()
