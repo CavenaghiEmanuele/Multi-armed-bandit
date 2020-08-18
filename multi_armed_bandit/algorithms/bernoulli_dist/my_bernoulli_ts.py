a3e1267c00fb99f41a3e8557877ce817065444bb
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-08-18 19:22:17 +0200

    Update my bernoulli ts

--- multi_armed_bandit/algorithms/bernoulli_dist/my_bernoulli_ts.py
+++ multi_armed_bandit/algorithms/bernoulli_dist/my_bernoulli_ts.py
@@ -14,41 +14,31 @@ class MyBernoulliTS(BernoulliAlgo):
     _n: int
     _alpha_0: float
     _beta_0: float
-    _gamma: np.ndarray
-    _gamma_0: np.ndarray
+    _gamma: float
 
-    def __init__(self, n_arms: int, gamma: float = 0.9, alpha_0: float = 1, beta_0: float = 1, decay_rate: float = 0.99, threshold: float = 0.1, n: int = 10):
+    def __init__(self, n_arms: int, gamma: float = 0.9, alpha_0: float = 1, beta_0: float = 1, threshold: float = 0.1, n: int = 10):
         super().__init__(n_arms=n_arms)
         self._alpha_0 = alpha_0
         self._beta_0 = beta_0
         self._threshold = threshold
-        self._decay_rate = decay_rate
         self._last_reward_trace = {a : [] for a in range(n_arms)}
         self._n = n
-        if isinstance(gamma, float):
-            self._gamma = np.ones(n_arms) * gamma
-            self._gamma_0 = np.ones(n_arms) + gamma
-        else:
-            self._gamma = gamma
-            self._gamma_0 = gamma
+        self._gamma = gamma
 
     def __repr__(self):
         return "My Thompson Sampling Bernoulli"
 
     def update_estimates(self, action: int, reward: int) -> None:
-        for a in range(self._n_arms):
-            self._betas[a] *= self._gamma[a]
+        self._betas *= self._gamma
 
         self.update_last_reward_trace(action, reward)
         tmp_mean = self._last_reward_trace[action].count(1) / len(self._last_reward_trace[action])
         distance = abs(self._betas[action][0] / (self._betas[action][0] + self._betas[action][1]) - tmp_mean)
 
         if distance > self._threshold:
-            self._betas[action][0] = self._last_reward_trace[action].count(1) # Reward == 1
-            self._betas[action][1] = self._last_reward_trace[action].count(0) # Reward == 0
-            self._gamma[action] = self._gamma_0[action]
+            self._betas[action][0] = 1 + self._last_reward_trace[action].count(1) # Reward == 1
+            self._betas[action][1] = 1 + self._last_reward_trace[action].count(0) # Reward == 0
         else:
-            self._gamma[action] /= self._decay_rate
             if reward == 0:
                 self._betas[action][1] += 1  # Update beta
             else:  # Reward == 1
