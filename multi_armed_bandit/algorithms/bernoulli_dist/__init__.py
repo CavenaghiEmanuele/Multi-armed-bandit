34782edc38279aea4685ef41873b80f776a7493e
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-08-29 23:26:51 +0200

    Add Bernoulli sliding window ts algorithm

--- multi_armed_bandit/algorithms/bernoulli_dist/__init__.py
+++ multi_armed_bandit/algorithms/bernoulli_dist/__init__.py
@@ -3,4 +3,5 @@ from .bernoulli_greedy import BernoulliGreedy
 from .bernoulli_ts import BernoulliThompsonSampling
 from .bernoulli_ucb import BernoulliUCB
 from .dynamic_bernoulli_ts import DynamicBernoulliTS
+from .bernoulli_sw_ts import BernoulliSlidingWindowTS
 from .my_bernoulli_ts import MyBernoulliTS
---
+++ multi_armed_bandit/algorithms/bernoulli_dist/bernoulli_sw_ts.py
@@ -0,0 +1,46 @@
+from .bernoulli_algo import BernoulliAlgo
+
+import numpy as np
+from numpy.random import beta
+from typing import List, Tuple
+
+
+'''
+Sliding-Window Thompson Sampling Algorithm
+From Paper: "Sliding-Window Thompson Sampling for Non-Stationary Settings"
+'''
+class BernoulliSlidingWindowTS(BernoulliAlgo):
+
+    _n: int
+    _last_n_action: List[Tuple]
+
+    def __init__(self, n_arms: int, n: int):
+        super().__init__(n_arms)
+        self._n = n
+        self._last_n_action = []
+    
+    def __repr__(self):
+        return "Sliding Window Thompson Sampling Bernoulli"
+
+    def update_estimates(self, action: int, reward: int) -> None:
+        self.update_last_n_action_trace(action, reward)
+        
+        if reward == 0:
+            self._betas[action][1] += 1  # Update beta
+        else:  # Reward == 1
+            self._betas[action][0] += 1  # Update alpha
+
+    def update_last_n_action_trace(self, action: int, reward: int):
+        if len(self._last_n_action) >= self._n:
+            tmp_action, tmp_reward = self._last_n_action.pop(0)
+            if tmp_reward == 0 and self._betas[tmp_action][1] > 1:
+                self._betas[tmp_action][1] -= 1  # Update beta
+            elif tmp_reward == 1 and self._betas[tmp_action][0] > 1:  # Reward == 1
+                self._betas[tmp_action][0] -= 1  # Update alpha
+        
+        self._last_n_action.append((action, reward))
+
+    def select_action(self) -> int:
+        samples = [beta(a=self._betas[a][0], b=self._betas[a][1])
+                   for a in range(self._n_arms)]
+        return np.argmax(samples)
