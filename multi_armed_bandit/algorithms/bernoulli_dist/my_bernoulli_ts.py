a48dae78e06ec4427d80c57c80cf97ed71fcfb27
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-08-31 23:11:34 +0200

    Update my bernoulli TS

--- multi_armed_bandit/algorithms/bernoulli_dist/my_bernoulli_ts.py
+++ multi_armed_bandit/algorithms/bernoulli_dist/my_bernoulli_ts.py
@@ -3,7 +3,6 @@ import numpy as np
 
 from numpy.random import beta
 from typing import Dict
-from copy import deepcopy
 
 from . import BernoulliAlgo
 
@@ -11,17 +10,15 @@ from . import BernoulliAlgo
 class MyBernoulliTS(BernoulliAlgo):
 
     _last_reward_trace: Dict
+    _tmp_betas: np.ndarray
     _n: int
-    _alpha_0: float
-    _beta_0: float
     _gamma: float
 
-    def __init__(self, n_arms: int, gamma: float = 0.9, alpha_0: float = 1, beta_0: float = 1, threshold: float = 0.1, n: int = 10):
+    def __init__(self, n_arms: int, gamma: float = 0.9, threshold: float = 0.1, n: int = 10):
         super().__init__(n_arms=n_arms)
-        self._alpha_0 = alpha_0
-        self._beta_0 = beta_0
         self._threshold = threshold
         self._last_reward_trace = {a : [] for a in range(n_arms)}
+        self._tmp_betas = np.ones(shape=(n_arms, 2))
         self._n = n
         self._gamma = gamma
 
@@ -31,28 +28,31 @@ class MyBernoulliTS(BernoulliAlgo):
     def update_estimates(self, action: int, reward: int) -> None:
         self._betas *= self._gamma
 
-        self.update_last_reward_trace(action, reward)
-        tmp_mean = self._last_reward_trace[action].count(1) / len(self._last_reward_trace[action])
-        distance = abs(self._betas[action][0] / (self._betas[action][0] + self._betas[action][1]) - tmp_mean)
+        self.update_tmp_betas(action, reward)
+        tmp_mean = self._tmp_betas[action][1] / (self._tmp_betas[action][1] + self._tmp_betas[action][0])
+        long_mean = self._betas[action][1] / (self._betas[action][1] + self._betas[action][0])
+        distance = abs(long_mean - tmp_mean)
 
-        if distance > self._threshold and len(self._last_reward_trace[action]) > self._n:
-            self._betas[action][0] = 1 + self._last_reward_trace[action].count(1) # Reward == 1
-            self._betas[action][1] = 1 + self._last_reward_trace[action].count(0) # Reward == 0
+        if distance > self._threshold and len(self._last_reward_trace[action]) >= self._n:
+            self._betas[action] = self._tmp_betas[action]
         else:
-            if reward == 0:
-                self._betas[action][1] += 1  # Update beta
-            else:  # Reward == 1
-                self._betas[action][0] += 1  # Update alpha
- 
-        for a in range(self._n_arms):
-            self._mean_trace[a].append(self._betas[a][0] / (self._betas[a][0] + self._betas[a][1]))
+            self._betas[action][reward] += 1
 
-    def update_last_reward_trace(self, action: int, reward: int):
+        for a in range(self._n_arms):
+            self._mean_trace[a].append(self._betas[a][1] / (self._betas[a][1] + self._betas[a][0]))
+
+    def update_tmp_betas(self, action: int, reward: int):
         if len(self._last_reward_trace[action]) >= self._n:
-            self._last_reward_trace[action].pop(0)
+            tmp_reward = self._last_reward_trace[action].pop(0)
+            self._tmp_betas[action][tmp_reward] -= 1
+
+        self._tmp_betas[action][reward] += 1
         self._last_reward_trace[action].append(reward)
 
     def select_action(self) -> int:
-        samples = [beta(a=self._betas[a][0] + self._alpha_0, b=self._betas[a][1] + self._beta_0)
+        samples = [beta(a=self._betas[a][1], b=self._betas[a][0])
                    for a in range(self._n_arms)]
-        return np.argmax(samples)
+        tmp_samples = [beta(a=self._tmp_betas[a][1], b=self._tmp_betas[a][0])
+                   for a in range(self._n_arms)]
+        
+        return np.argmax([max(samples[a], tmp_samples[a]) for a in range(self._n_arms)])
