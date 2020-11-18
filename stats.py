0b98802d1d5965e7851b26ed721aa0677070113a
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-11-19 00:00:19 +0100

    Update stats with csv writer

--- stats.py
+++ stats.py
@@ -1,14 +1,15 @@
-from multi_armed_bandit import session
-from typing import Dict, Tuple
-from multi_armed_bandit.algorithms.bernoulli_dist import min_dsw_ts
 import multi_armed_bandit as mab
 
-from multiprocessing import Pool, cpu_count
-
 import matplotlib.pyplot as plt
+import pandas as pd
+
+from multiprocessing import Pool, cpu_count
+from typing import Dict, Tuple
 
 
-def non_stationary_bernoulli_custom(n_arms, n_test, test_number:int) -> Tuple:
+
+
+def custom_environments(n_arms, n_test, test_number:int) -> Tuple:
     # Build environment
     replay = {}
     if test_number == 1:
@@ -47,12 +48,11 @@ def non_stationary_bernoulli_custom(n_arms, n_test, test_number:int) -> Tuple:
     # Run session
     replay_session.run(n_step=1000, n_test=n_test, use_replay=True)
 
-    return replay_session._regrets, replay_session._real_reward_trace
+    return pd.DataFrame.from_dict(replay_session._regrets), pd.DataFrame.from_dict(replay_session._real_reward_trace)
 
 
-def multiple_env(n_arms, n_step, n_test, n_envs, cpus:int=cpu_count()) -> Dict:
+def multiple_env(n_arms, n_step, n_test, n_envs, cpus:int=cpu_count()) -> pd.DataFrame:
     results = {}
-    parms = [(n_arms, n_step, n_test) for _ in range(n_envs)]
     for prob_of_change in [0.001, 0.002, 0.003, 0.004, 0.005, 0.01, 0.015, 0.02]:
         rewards = {"Oracle": 0,
                 "Thompson Sampling Bernoulli": 0,
@@ -62,7 +62,7 @@ def multiple_env(n_arms, n_step, n_test, n_envs, cpus:int=cpu_count()) -> Dict:
                 "Min d-sw TS Bernoulli": 0,
                 "Mean d-sw TS Bernoulli":0
                 }
-
+        parms = [(n_arms, n_step, n_test, prob_of_change) for _ in range(n_envs)]
         pool = Pool(cpus)
         result = pool.starmap(_multiple_env, parms)
         pool.close()
@@ -71,13 +71,19 @@ def multiple_env(n_arms, n_step, n_test, n_envs, cpus:int=cpu_count()) -> Dict:
         for res in result:
             for agent in res:
                 rewards[str(agent)] += res[agent]
-        results.update({prob_of_change:rewards})
+        results.update({prob_of_change:[rewards[item] for item in rewards]})
 
-    return results
+    return pd.DataFrame.from_dict(results, orient='index', columns=["Oracle",
+                                                                    "Thompson Sampling Bernoulli",
+                                                                    "Dynamic Thompson Sampling Bernoulli",
+                                                                    "Sliding Window Thompson Sampling Bernoulli",
+                                                                    "Max d-sw TS Bernoulli",
+                                                                    "Min d-sw TS Bernoulli",
+                                                                    "Mean d-sw TS Bernoulli"])
 
-def _multiple_env(n_arms, n_step, n_test):
+def _multiple_env(n_arms, n_step, n_test, prob_of_change):
     # Build environment
-    env = mab.BernoulliDynamicBandit(n_arms, prob_of_change=0.002, fixed_action_prob=0.0, save_replay=True)
+    env = mab.BernoulliDynamicBandit(n_arms, prob_of_change=prob_of_change, fixed_action_prob=0.0, save_replay=True)
 
     # Generate replay
     session = mab.Session(env, [])
@@ -105,16 +111,16 @@ def _multiple_env(n_arms, n_step, n_test):
     return results
 
 
-def min_max_comparison(n_arms, n_step, n_test, n_envs, cpus:int=cpu_count()):
+def min_max_mean_comparison(n_arms, n_step, n_test, n_envs, cpus:int=cpu_count()) -> pd.DataFrame:
     results = {}
-    parms = [(n_arms, n_step, n_test) for _ in range(n_envs)]
+    
     for prob_of_change in [0.001, 0.002, 0.003, 0.004, 0.005, 0.01, 0.015, 0.02]:
         rewards = {"Oracle": 0,
                 "Min d-sw TS Bernoulli": 0,
                 "Max d-sw TS Bernoulli": 0,
                 "Mean d-sw TS Bernoulli": 0
                 }
-
+        parms = [(n_arms, n_step, n_test, prob_of_change) for _ in range(n_envs)]
         pool = Pool(cpus)
         result = pool.starmap(_min_max_comparison, parms)
         pool.close()
@@ -123,13 +129,17 @@ def min_max_comparison(n_arms, n_step, n_test, n_envs, cpus:int=cpu_count()):
         for res in result:
             for agent in res:
                 rewards[str(agent)] += res[agent]
-        results.update({prob_of_change:rewards})
+        
+        results.update({prob_of_change:[rewards[item] for item in rewards]})
 
-    print(results)
-    
-def _min_max_comparison(n_arms, n_step, n_test):
+    return pd.DataFrame.from_dict(results, orient='index', columns=["Oracle", 
+                                                                       "Min d-sw TS Bernoulli", 
+                                                                       "Max d-sw TS Bernoulli", 
+                                                                       "Mean d-sw TS Bernoulli"])
+
+def _min_max_comparison(n_arms, n_step, n_test, prob_of_change):
     # Build environment
-    env = mab.BernoulliDynamicBandit(n_arms, prob_of_change=0.002, fixed_action_prob=0.0, save_replay=True)
+    env = mab.BernoulliDynamicBandit(n_arms, prob_of_change=prob_of_change, fixed_action_prob=0.0, save_replay=True)
 
     # Generate replay
     session = mab.Session(env, [])
@@ -160,8 +170,19 @@ if __name__ == "__main__":
     n_step = 100 #1000
     n_test = 3 #30
     n_envs = 10 #1000
-
-    #result = multiple_env(n_arms, n_step, n_test, n_envs, cpus=4)
-    #result = min_max_comparison(n_arms, n_step, n_test, n_envs, cpus=4)
-    regret, real_reward_trace = non_stationary_bernoulli_custom(n_arms, n_test=10 ,test_number=1)
+    
+    '''
+    result = multiple_env(n_arms, n_step, n_test, n_envs, cpus=4)
+    result.to_csv("results/Multiple_env.csv")
+    '''
+    '''
+    result = min_max_mean_comparison(n_arms, n_step, n_test, n_envs, cpus=4)
+    result.to_csv("results/dsw_ts_comparison.csv")
+    '''
+    
+    test_number = 1
+    regret, real_reward_trace = custom_environments(n_arms, n_test=10 ,test_number=test_number)
+    regret.to_csv("results/custom_test_" + str(test_number) + "_regret.csv")
+    real_reward_trace.to_csv("results/custom_test_" + str(test_number) + "_real_reward_trace.csv")
+    
 
