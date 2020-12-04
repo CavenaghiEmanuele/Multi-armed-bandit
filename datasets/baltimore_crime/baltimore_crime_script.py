8827b614d460d69d196f7d036749440b322ada5d
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-12-04 21:18:39 +0100

    Update baltimore_crime_script

--- datasets/baltimore_crime/baltimore_crime_script.py
+++ datasets/baltimore_crime/baltimore_crime_script.py
@@ -3,12 +3,14 @@ import pandas as pd
 
 if __name__ == '__main__':
     
-    dataset = pd.read_csv('baltimore_crime.csv', parse_dates=['CrimeDate'])
+    dataset = pd.read_csv('baltimore_crime.csv', parse_dates=['CrimeDate'], keep_default_na=False)
     dataset.drop(['CrimeCode', 'Premise', 'Description', 'Latitude', 'Longitude', 
                   'Location 1', 'vri_name1', 'Total Incidents', 'Post', 
                   'Neighborhood', 'Inside/Outside', 'Weapon', 'Location'], 
                  axis=1, 
                  inplace=True)
     
-    dataset.to_csv('modified_baltimore_crime.csv', index=False)
+    dataset.drop(dataset[dataset.District == ''].index, inplace=True)
+    reversed_df = dataset.iloc[::-1]
+    reversed_df.to_csv('modified_baltimore_crime.csv', index=False)
     
