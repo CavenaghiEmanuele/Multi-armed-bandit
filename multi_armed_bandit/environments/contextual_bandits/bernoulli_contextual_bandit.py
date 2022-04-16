246fa4243b8edeed467937c884dd32f73d64f8de
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2022-04-16 11:01:15 +0200

    Update bernoulli contextual bandit

--- multi_armed_bandit/environments/contextual_bandits/bernoulli_contextual_bandit.py
+++ multi_armed_bandit/environments/contextual_bandits/bernoulli_contextual_bandit.py
@@ -1,8 +1,5 @@
-from matplotlib.style import context
 import numpy as np
-import matplotlib.pyplot as plt
 from numpy.random import uniform, binomial
-from typing import List
 
 from .. import MultiArmedBandit
 
@@ -23,8 +20,12 @@ class BernoulliContextualBandit(MultiArmedBandit):
             raise Exception(
                 "Length of probabilities vector must be the same of number of arms")
         
-        self._context = np.random.rand(context_dim)
-        self._best_action = np.argmax([1/(1 + np.exp(sum(self._probabilities[a] * self._context))) for a in range(n_arms)])
+        self._context = uniform(-1,1,context_dim)
+        self._best_action = np.argmax(
+            [
+                max(-np.cos(-sum(self._probabilities[a] * self._context)), np.cos(-sum(self._probabilities[a] * self._context)))
+                for a in range(self._probabilities.shape[0])
+            ])
        
 
     def __repr__(self):
@@ -36,24 +37,24 @@ class BernoulliContextualBandit(MultiArmedBandit):
 
     def do_action(self, action: int):
         f_x = sum(self._probabilities[action] * self._context)
-        return binomial(size=1, n=1, p=1/(1 + np.exp(-f_x)))
+        return binomial(size=1, n=1, p=max(-np.cos(f_x), np.cos(f_x)))
 
     def best_action_mean(self):
         f_x = sum(self._probabilities[self._best_action] * self._context)
-        return 1/(1 + np.exp(-f_x))
+        return max(-np.cos(f_x), np.cos(f_x))
 
     def get_best_action(self):
         return self._best_action
 
     def action_mean(self, action: int):
         f_x = sum(self._probabilities[action] * self._context)
-        return 1/(1 + np.exp(-f_x))
+        return max(-np.cos(f_x), np.cos(f_x))
 
     def update_context(self):
-        self._context = np.random.rand(self._probabilities.shape[1])
+        self._context = self._context = uniform(-1,1,self._probabilities.shape[1])
         self._best_action = np.argmax(
             [
-                1/(1 + np.exp(-sum(self._probabilities[a] * self._context))) 
+                max(-np.cos(-sum(self._probabilities[a] * self._context)), np.cos(-sum(self._probabilities[a] * self._context)))
                 for a in range(self._probabilities.shape[0])
             ])
         return
