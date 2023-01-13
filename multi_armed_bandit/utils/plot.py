949bfcb8bff5aa4b034f53c79b6d6a1562dbfd52
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2023-01-13 14:10:12 +0100

    Update action and state plots to lineplots in time

--- multi_armed_bandit/utils/plot.py
+++ multi_armed_bandit/utils/plot.py
@@ -9,11 +9,15 @@ def plot_cumulative_reward(data: pd.DataFrame, errorbar=('ci', 95)):
     plt.show()
 
 def plot_performed_actions(data: pd.DataFrame):
-    sns.displot(data=data, x='action', hue='agent', multiple='dodge')
+    # plus 1 becouse cumcount starts from 0
+    data['cum_action'] = data.groupby(['agent', 'experiment', 'action'])['action'].cumcount()+1
+    sns.relplot(data=data, x='step', y='cum_action', hue='action', row='agent', kind='line')
     plt.show()
 
 def plot_visited_states(data: pd.DataFrame):
-    sns.displot(data=data, x='state', hue='agent', multiple='dodge')
+    # plus 1 becouse cumcount starts from 0
+    data['cum_state'] = data.groupby(['agent', 'experiment', 'state'])['state'].cumcount()+1
+    sns.relplot(data=data, x='step', y='cum_state', hue='state', row='agent', kind='line')
     plt.show()
 
 def plot_cumulative_regret(data: pd.DataFrame):
