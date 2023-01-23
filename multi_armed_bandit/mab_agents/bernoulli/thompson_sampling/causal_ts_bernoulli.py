17ea8aa159a435bd10b332242ed9e534107dae02
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2023-01-23 19:17:06 +0100

    Use bayesian ts as superclass for causal ts

--- multi_armed_bandit/mab_agents/bernoulli/thompson_sampling/causal_ts_bernoulli.py
+++ multi_armed_bandit/mab_agents/bernoulli/thompson_sampling/causal_ts_bernoulli.py
@@ -1,41 +1,21 @@
-import numpy as np
-import networkx as nx
-import pylab as plt
-
 from typing import List, Dict
-from collections import defaultdict
-from copy import deepcopy
-from pandas import DataFrame
 from numpy.random import beta
 from pgmpy.models.BayesianNetwork import BayesianNetwork
-from pgmpy.factors.discrete.CPD import TabularCPD
 from pgmpy.inference import CausalInference
 
-from ...agent import Agent
+from .bayesian_ts_bernoulli import BayesianTSBernoulli
 from ....utils import from_dict_to_str
 
 
-class CausalTSBernoulli(Agent):
+class CausalTSBernoulli(BayesianTSBernoulli):
 
-    _bn: BayesianNetwork
     _inference_engine: CausalInference
-    _n_observations: int
-    _obs_for_context: defaultdict
     _adjustment_set: set
 
     def __init__(self, id:str, actions: List[str], contexts:Dict, bn:BayesianNetwork):
-        super().__init__(id, actions, contexts)
-        self._bn = deepcopy(bn)
-        self._init_uniform_cpds()
-        self._n_observations = 1
-        self._obs_for_context = defaultdict(int)
+        super().__init__(id, actions, contexts, bn)
         self._inference_engine = CausalInference(self._bn)
         self._adjustment_set = self._inference_engine.get_minimal_adjustment_set('X', 'Y')
-
-    def update_estimates(self, context:int, action: str, reward: int) -> None:
-        self._bn.fit_update(DataFrame([context|{'X':action, 'Y':reward}]), n_prev_samples=self._n_observations)
-        self._n_observations += 1
-        self._obs_for_context[from_dict_to_str(context|{'X': action})] += 1
  
     def select_action(self, context:int, available_actions:List[str]) -> str:
         samples = {}
@@ -50,45 +30,3 @@ class CausalTSBernoulli(Agent):
             obs = self._obs_for_context[from_dict_to_str(context|{'X': a})] + 1
             samples.update({a:beta(a=prob*obs, b=(1-prob)*obs)})
         return max(samples, key=samples.get)
-
-
-    def plot_bn(self) -> None:
-        pos = nx.circular_layout(self._bn)
-        nx.draw(self._bn, node_color='#00b4d9', pos=pos, with_labels=True)
-        plt.show()
-
-    def print_cpds(self, node:str=None) -> None:
-        if node != None:
-            print(self._bn.get_cpds(node))
-            return
-        for cpd in self._bn.get_cpds():
-            print(cpd)
-
-
-    def _init_uniform_cpds(self):
-        variables = self._contexts|{'X':self._actions, 'Y':[0,1]}
-
-        for context in variables.keys():
-            parents = list(self._bn.predecessors(context))
-
-            if len(parents) == 0:
-                values = np.ones((len(variables[context]), 1))
-                values = values / np.sum(values, axis=0)
-                self._bn.add_cpds(TabularCPD(
-                    variable=context,
-                    variable_card=len(variables[context]),
-                    values=values,
-                    state_names=variables,
-                ))
-            else:
-                values = np.ones((len(variables[context]), np.product([len(variables[parent]) for parent in parents])))
-                values = values / np.sum(values, axis=0)
-
-                self._bn.add_cpds(TabularCPD(
-                    variable=context,
-                    variable_card=len(variables[context]),
-                    values=values,
-                    evidence=parents,
-                    evidence_card=[len(variables[parent]) for parent in parents],
-                    state_names=variables,
-                ))
